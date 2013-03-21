678e9c83cec1a72cfeecc3ff54435b4e2ff123ea
chadgauth <chadgauth@gmail.com>
2013-03-21 17:49:43 -0400

    Updated the speed and fixed navigation to direct schedule. **Up next automatic current semester.

--- gutools/src/com/example/webviewexample/DataPullActivity.java
+++ gutools/src/com/example/webviewexample/DataPullActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 public class DataPullActivity extends Activity {
@@ -35,70 +36,65 @@ public class DataPullActivity extends Activity {
 		    		 mHandler.postDelayed(new Runnable() {
 		    	            public void run() {
 		    	            	login();
-		    	            	pb.setProgress(1);
 		    	            }
-		    	        }, 1000);
+		    	        }, 500);
 		    	 }
 		    	 else{
+		    		 pb.setProgress(1);
 		    		//All these are sequential once logged in processHTML is used to pass variables back to Android's interface 
 		    		 webView.loadUrl("https://guxpress.gannon.edu/GUXpress/colleague?TOKENIDX=5787208423&SS=LGRQ");
-		    		 mHandler.postDelayed(new Runnable() {
-		    	            public void run() {
-		    	            	//LOG IN
-		    	            	webView.loadUrl("javascript:document.getElementById('USER_NAME').value=\"" + getIntent().getStringExtra("username") + "\"");
+		    		 webView.setWebViewClient(new WebViewClient() {
+		    			    public void onPageFinished(WebView view, String url) {
+		    			    	webView.loadUrl("javascript:document.getElementById('USER_NAME').value=\"" + getIntent().getStringExtra("username") + "\"");
 		    	            	webView.loadUrl("javascript:document.getElementById('CURR_PWD').value=\""+ getIntent().getStringExtra("password") +"\"");
 		    	            	webView.loadUrl("javascript:document.getElementsByClassName('shortButton')[0].click()");
 		    	            	pb.setProgress(2);
-		    	            }
-		    	        }, 3000);
-		    		 mHandler.postDelayed(new Runnable() {
-		    	            public void run() {
-		    	            	//A function is passed to click on the next
-		    	            	webView.loadUrl("javascript:function test(){ var link = document.getElementsByClassName('WBST_Bars')[0].href; window.location.href=link;}");
-		    	            	webView.loadUrl("javascript:test();");
-		    	            	pb.setProgress(3);
-		    	            }
-		    	        }, 6000);
-		    		 mHandler.postDelayed(new Runnable() {
-		    	            public void run() {
-		    	            	webView.loadUrl("javascript:function test(){ var link = document.getElementsByClassName('left')[0].getElementsByTagName('li')[5].getElementsByTagName('a')[0].href; window.location.href=link;}");
-		    	            	webView.loadUrl("javascript:test();");
-		    	            	pb.setProgress(4);
-		    	            	
-		    	            }
-		    	        }, 9000);
-		    		 mHandler.postDelayed(new Runnable() {
-		    	            public void run() {
-		    	            	webView.loadUrl("javascript:document.getElementById('VAR4').selectedIndex=2");
-		    	            	webView.loadUrl("javascript:document.getElementsByClassName('shortButton')[0].click()");
-		    	            	pb.setProgress(5);
-		    	            }
-		    	        }, 11000);
-		    		 mHandler.postDelayed(new Runnable() {
-		    	            public void run() {
-		    	            	webView.setVisibility(View.VISIBLE);
-		    	            	pb.setVisibility(View.INVISIBLE);
-		    	            }
-		    	        }, 13000);
-		    		 //webView.loadUrl("https://guxpress.gannon.edu/GUXpress/colleague?TOKENIDX=180218265&SS=LGRQ&URL");
-		    		// Toast.makeText(getApplicationContext(), "Page is " + loaded, Toast.LENGTH_LONG).show();
+		    	            	webView.setWebViewClient(new WebViewClient() {
+				    			    public void onPageFinished(WebView view, String url) {
+				    			    	webView.loadUrl("javascript:function test(){ var link = document.getElementsByClassName('WBST_Bars')[0].href; window.location.href=link;}");
+				    	            	webView.loadUrl("javascript:test();");
+				    	            	pb.setProgress(3);
+				    	            	webView.setWebViewClient(new WebViewClient() {
+						    			    public void onPageFinished(WebView view, String url) {
+						    	            	webView.loadUrl("javascript:function test(){ var link = document.getElementsByClassName('left')[0].getElementsByTagName('li')[5].getElementsByTagName('a')[0].href; window.location.href=link;}");
+						    	            	webView.loadUrl("javascript:test();");
+						    	            	pb.setProgress(4);
+						   		    		 	webView.setWebViewClient(new WebViewClient() {
+								    			    public void onPageFinished(WebView view, String url) {
+								    			    	pb.setProgress(5);
+								    	            	webView.loadUrl("javascript:document.getElementById('VAR4').selectedIndex=1");
+								    	            	webView.loadUrl("javascript:document.getElementsByClassName('shortButton')[0].click()");
+								    	            	webView.setWebViewClient(new WebViewClient() {
+										    			    public void onPageFinished(WebView view, String url) {
+										    	            	webView.setVisibility(View.VISIBLE);
+										    	            	pb.setVisibility(View.INVISIBLE);
+										    			    }
+										    			});
+								    			    }
+								    			});
+						    			    }
+						    			});
+				    			    }
+				    			});
+		    			    }
+		    			});
 		    	 }
 		    }
 		}
 
 		// Register a new JavaScript interface called HTMLOUT
 		webView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+		
 		eWebViewC = new ExtendedWebViewClient();
 		eWebViewC.setUrl("https://guxpress.gannon.edu/GUXpress/colleague?TYPE=M&PID=CORE-WBMAIN&TOKENIDX=");
 		webView.setWebViewClient(eWebViewC);
-
 		webView.loadUrl(guXpress);
 		
 		mHandler.postDelayed(new Runnable() {
             public void run() {
             	login();
             }
-        }, 1000);
+        }, 500);
 		
 
 		
@@ -114,7 +110,7 @@ public class DataPullActivity extends Activity {
 	            public void run() {
 	            	login();
 	            }
-	        }, 1000);
+	        }, 500);
 		}
 		//webView.loadUrl("javascript:document.getElementById('USER_NAME').value=\"gauthier001\"");
 		//webView.loadUrl("javascript:document.getElementById('CURR_PWD').value=\"t3so7sMgnn\"");

9635afd294e78701fa9369111941112ba8503aca
chadgauth <chadgauth@gmail.com>
2013-04-17 14:16:35 -0400

    added cardview to the classes!

---
+++ gutools/src/com/gannon/gutools/activities/MyCard.java
@@ -0,0 +1,30 @@
+package com.gannon.gutools.activities;
+
+import org.holoeverywhere.LayoutInflater;
+import org.holoeverywhere.widget.TextView;
+
+import android.content.Context;
+import android.view.View;
+
+import com.fima.cardsui.objects.Card;
+import com.gannon.gutools.dev.R;
+
+public class MyCard extends Card {
+
+	public MyCard(String title){
+		super(title);
+	}
+
+	@Override
+	public View getCardContent(Context context) {
+		View view = LayoutInflater.from(context).inflate(R.layout.card_ex, null);
+		((TextView) view.findViewById(R.id.title)).setText(title);
+
+		
+		return view;
+	}
+
+	
+	
+	
+}
--- gutools/src/com/gannon/gutools/fragments/ClassFragment.java
+++ gutools/src/com/gannon/gutools/fragments/ClassFragment.java
@@ -2,17 +2,51 @@ package com.gannon.gutools.fragments;
 
 import org.holoeverywhere.app.Fragment;
 
+import com.gannon.gutools.activities.MyCard;
+import com.fima.cardsui.objects.CardStack;
+import com.fima.cardsui.views.CardUI;
 import com.gannon.gutools.dev.R;
 
 import android.os.Bundle;
-import org.holoeverywhere.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.holoeverywhere.LayoutInflater;
+
 public class ClassFragment extends Fragment {
+
+	private CardUI mCardView;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		// construct the RelativeLayout
-		return inflater.inflate(R.layout.events);
+		View V = inflater.inflate(R.layout.events);
+		mCardView = (CardUI) V.findViewById(R.id.cardsview);
+		mCardView.setSwipeable(false);
+
+		// add AndroidViews Cards
+		mCardView.addCard(new MyCard("Get the CardsUI view"));
+		mCardView.addCardToLastStack(new MyCard("for Android at"));
+		MyCard androidViewsCard = new MyCard("www.androidviews.net");
+		mCardView.addCardToLastStack(androidViewsCard);
+
+		// add one card, and then add another one to the last stack.
+		mCardView.addCard(new MyCard("2 cards"));
+		mCardView.addCardToLastStack(new MyCard("2 cards"));
+
+
+		// create a stack
+		CardStack stack = new CardStack();
+		stack.setTitle("title test");
+
+		// add 3 cards to stack
+		stack.add(new MyCard("3 cards"));
+		stack.add(new MyCard("3 cards"));
+		stack.add(new MyCard("3 cards"));
+
+		// add stack to cardView
+		mCardView.addStack(stack);
+
+		// draw cards
+		mCardView.refresh();
+		return V;
 	}
 }
